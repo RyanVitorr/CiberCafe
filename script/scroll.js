93cde4a9d0dea6b7c166bf5eb7011a089dd654ac
Ryan  Vitor <127523720+RyanVitorr@users.noreply.github.com>
2023-10-20 01:04:38 -0300

    atualizei o mediaQuery

--- script/scroll.js
+++ script/scroll.js
@@ -1,6 +1,10 @@
-
-
 window.addEventListener('scroll', () => {
     const botaoUp = document.getElementById('botaoUp');
     botaoUp.classList.toggle('scroll', window.scrollY > 550);
+
+    const header = document.getElementById('containerHeader');
+    header.classList.toggle('scroll', window.scrollY > 10);
 })
+
+
+
