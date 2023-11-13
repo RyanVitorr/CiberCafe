39b9c13e302caa9dbedfb58ae4d42d7b751c1844
Ryan  Vitor <127523720+RyanVitorr@users.noreply.github.com>
2023-11-13 18:32:03 -0300

    .

--- script/carrinho.js
+++ script/carrinho.js
@@ -1,12 +1,11 @@
+/* global */
+const containerEscolherMesas = document.querySelector(".mesas-card");
+const divAddCardCarrinho = document.querySelector("#conteudo-item-carrinho");
 
 /* conteudo da mesa de grupo*/
 
-const containerEscolherMesas = document.querySelector(".mesas-card");
 const bntMesaGrupo = document.querySelector("#mesa-grupo");
 const sectionAluguel = document.querySelector("#aluguel");
-
-
-
 const listaMesasDeGrupo = [
     {
         idMesa: "1",
@@ -33,7 +32,7 @@ const listaMesasDeGrupo = [
 function escolhaMesaGrupo () {
     for (const itensMesas of listaMesasDeGrupo) {
         const mesas = 
-           `<div class="mesas" id="${itensMesas.idMesa}" onclick="addMesaEscolhida()">
+           `<div class="mesas" id="${itensMesas.idMesa}" onclick="addMesaEscolhidaGrupo()">
                 <i class="${itensMesas.iconeMesa}"></i>
             </div>`;
     
@@ -48,9 +47,10 @@ bntMesaGrupo.addEventListener("click", () => {
     escolhaMesaGrupo()
 });
 
-// ao clicar na mesa que deseja 
-const divAddCardCarrinho = document.querySelector("#conteudo-item-carrinho");
-const escolheuMesa = `
+
+// ao clicar na mesa de grupo que deseja do 
+
+const escolheuMesaGrupo = `
                         <div class="card-item-carrinho">
                             <div class="img-item-carrinho">
                                 <img src="#" alt="">
@@ -74,14 +74,96 @@ const escolheuMesa = `
                                 </div>
                             </div>
                         </div>`;
-var i = 0;
 
-function addMesaEscolhida() {
-    divAddCardCarrinho.innerHTML = escolheuMesa;
-    i = 1;
-    console.log(i);
+
+function addMesaEscolhidaGrupo() {
+    divAddCardCarrinho.innerHTML = escolheuMesaGrupo;
 }
 
+
+
+/* conteudo mesa solo */
+const bntMesaSolo = document.querySelector("#mesa-solo");
+
+const listaMesasSolo = [
+    {
+        idMesa: "1",
+        iconeMesa: "fa-solid fa-computer",
+        imgMesa: "fe",
+    },
+    {
+        idMesa: "2",
+        iconeMesa: "fa-solid fa-computer",
+        imgMesa: "fe",
+    },
+    {
+        idMesa: "3",
+        iconeMesa: "fa-solid fa-computer",
+        imgMesa: "fe",
+    },
+    {
+        idMesa: "4",
+        iconeMesa: "fa-solid fa-computer",
+        imgMesa: "fe",
+    },
+    {
+        idMesa: "5",
+        iconeMesa: "fa-solid fa-computer",
+        imgMesa: "fe",
+    },
+
+];
+
+function escolhaMesaSolo () {
+    for (const itensMesas of listaMesasSolo) {
+        const mesas = 
+           `<div class="mesas" id="${itensMesas.idMesa}" onclick="addMesaEscolhidaSolo()">
+                <i class="${itensMesas.iconeMesa}"></i>
+            </div>`;
+    
+        containerEscolherMesas.innerHTML += mesas;
+    };    
+};
+
+bntMesaSolo.addEventListener("click", () => {
+    if (sectionAluguel.style.display == "") {
+        sectionAluguel.style.display = "flex";
+    }
+    escolhaMesaSolo()
+});
+
+// ao clicar na mesa solo que deseja
+
+const escolheuMesaSolo = `
+                        <div class="card-item-carrinho">
+                            <div class="img-item-carrinho">
+                                <img src="#" alt="">
+                            </div>
+
+                            <div class="infos-item-carrinho">
+                                <div class="nome-item-carrinho">
+                                    <span>Mesa Solo 1</span>
+                                </div>
+
+                                <div class="bnt-carrinho">
+                                    <div class="bnt-pri">
+                                        <button class="bnt-item-carrinho"></button>
+                                        <div class="numero-hors-item-carrinho"><p>1</p></div>
+                                        <button class="bnt-item-carrinho"></button>
+                                    </div>
+
+                                    <div class="bnt-seg">
+                                        <button class="bnt-excluir" onclick="excluirItemCarrinho(event)"></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+
+                
+function addMesaEscolhidaSolo() {
+    divAddCardCarrinho.innerHTML = escolheuMesaSolo;
+};
+                        
 
 // ao clicar no botão de excluir item do carrinho 
 
